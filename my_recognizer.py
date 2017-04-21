d04304c53f805809f62c1185cb589d70bf5c2538
fmind <fmind@users.noreply.github.com>
2017-04-21 16:43:39 +0200

    Implemented my_recognizer.py

--- my_recognizer.py
+++ my_recognizer.py
@@ -1,5 +1,19 @@
+import logging
+import math
 import warnings
+from functools import partial
+
 from asl_data import SinglesData
+
+
+def safe_score(model, parameters):
+    """Compute the score of a model or return None."""
+    try:
+        return model.score(*parameters)
+    except Exception as e:
+        # logging.warn("({}) {}".format(e.__class__.__name__, str(e)))
+
+        return None
 
 
 def recognize(models: dict, test_set: SinglesData):
@@ -18,8 +32,23 @@ def recognize(models: dict, test_set: SinglesData):
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    probabilities = []
-    guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    def evaluate(models, params):
+        """Return the log-likelihood for params given a list of models."""
+        return {word: safe_score(model, params) or -math.inf for word, model in models.items()}
+
+    def predict(evaluation):
+        """Predict the best guess word given an evaluations dictionary."""
+        # key=evaluation.get: return the dict val
+        return max(evaluation, key=evaluation.get)
+
+    # convert the test set into a list of parameters
+    parameters = test_set.get_all_Xlengths().values()
+    evaluate_given_models = partial(evaluate, models)
+
+    # compute evaluation: log-likelihood / model (word) / parameters
+    evaluations = list(map(evaluate_given_models, parameters))
+    # compute prediction: max(evaluation.value) / parameters
+    predictions = list(map(predict, evaluations))
+
+    return evaluations, predictions
